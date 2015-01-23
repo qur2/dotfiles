3f97a7d9d3bd01446aced81b17bd62a280db80c6
Aurélien Scoubeau <aurelien.scoubeau@gmail.com>
2015-01-23 15:14:22 +0100

    Add aliases and some deps + comments.

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # install homebrew
+# http://brew.sh/#install
 if [[ -z "$(which brew)" ]]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -13,4 +14,16 @@ fi
 # install zsh
 if [[ -z "$(which zsh)" ]]; then
   brew install zsh
+fi
+
+# install nvm
+# https://github.com/creationix/nvm
+if [[ -z "$(which nvm)" ]]; then
+  curl https://raw.githubusercontent.com/creationix/nvm/v0.23.0/install.sh | bash
+fi
+
+# install virtualenv
+# https://github.com/brainsik/virtualenv-burrito
+if [[ -z "$(which virtualenv-burrito)" ]]; then
+  curl -sL https://raw.githubusercontent.com/brainsik/virtualenv-burrito/master/virtualenv-burrito.sh | $SHELL
 fi
